ea1aca308c09b6d6066363fb57411cff45c8e334
marcellosusanto <36285137+marcellosusanto@users.noreply.github.com>
2019-07-04 17:52:46 +0700

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -1,6 +1,6 @@
 def mapping_001(df) :
   '''
-  Mapping rating agar sesuai untuk modelling surprise
+  Mapping event agar sesuai untuk modelling surprise
   Digunakan pada dataset Retail Rocket
   Input :
    - df (Pandas.DataFrame) : Dataset yang digunakan untuk modelling
@@ -10,7 +10,7 @@ def mapping_001(df) :
   map_001 = {'view':0, 'addtocart':0, 'transaction':1}
   
   # Lakukan mapping 
-  df['rating'] = df['rating'].replace(map_001)
+  df['event'] = df['event'].replace(map_001)
   
   # Check berhasil atau tidak
   print('Mapping 0-0-1 selesai')
